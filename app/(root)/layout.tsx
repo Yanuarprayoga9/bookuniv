bb3a3070fac72bcdd4b0a3ced024bff2d69bec86
Yanuarprayoga9 <yanuarprayoga.stu@pnc.ac.id>
2025-02-06 09:47:22 +0700

    feat: header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/ui/Header'
 import React from 'react'
 
 type LayoutProps = {
@@ -7,6 +8,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
         <main className="root-container">
             <div className="mx-auto max-w-7xl">
+                <Header />
                 <div className="mt-20 pb-20">{children}</div>
             </div>
         </main>
