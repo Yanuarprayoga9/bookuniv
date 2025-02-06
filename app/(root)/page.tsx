728da34d8bc1e4523f6bdc6106e7b74016610153
Yanuarprayoga9 <yanuarprayoga.stu@pnc.ac.id>
2025-02-06 11:54:47 +0700

    feat: book list ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,18 @@
+import BookOverview from '@/components/BookOverview'
 import { Button } from '@/components/ui/button'
 import React from 'react'
+import { sampleBooks } from '../constants'
+import BookList from '@/components/BookList'
 
 const RootPage = () => {
   return (
     <div>
-        <Button>Root button</Button>
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
     </div>
   )
 }
